f5255e062229eab37079384fb5ea4e5143f83004
Jason Ehrlicher <jehrlich@uno.edu>
2024-01-27 16:56:31 -0600

    added weather icon and tooltips to historical data grid, which is connected to the backend

--- mini-project2/src/scenes/history/index.jsx
+++ mini-project2/src/scenes/history/index.jsx
@@ -11,121 +11,134 @@ import {
 } from "@mui/material";
 import Header from "../../components/Header";
 import { DataGrid } from "@mui/x-data-grid";
-import useParseCSV from "../../hooks/useParseCSV";
 import { tokens } from "../../theme";
+import Tooltip from '@mui/material/Tooltip';
 
 const History = () => {
-  // Access the theme to use in styling
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
-  // State for storing the selected year
   const [selectedYear, setSelectedYear] = useState("");
+  const [fetchedData, setFetchedData] = useState([]);
 
-  // Columns definition for DataGrid
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/rounds-played");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        const formattedData = Object.entries(data).map(
+          ([date, details], index) => ({
+            id: index,
+            Date: date,
+            Day: details.day,
+            "Rounds Played": details.roundsPlayed,
+            "Weather Icon": details.weatherIcon,
+            ...details
+          })
+        );
+        setFetchedData(formattedData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const columns = [
-    { field: "Date", headerName: "Date", width: 150 },
-    { field: "Day", headerName: "Day", width: 130 },
+    { field: "Date", headerName: "Date", flex: 1 },
+    { field: "Day", headerName: "Day", flex: 1 },
     {
       field: "Rounds Played",
       headerName: "Rounds Played",
       type: "number",
-      width: 130,
+      flex: 1,
+    },
+    {
+      field: "Weather Icon",
+      headerName: "Weather",
+      flex: 1,
+      renderCell: (params) => {
+        if (params.row.Date !== "Total") {
+          const iconUrl = `http://openweathermap.org/img/wn/${params.value}@2x.png`;
+
+          const tooltipContent = (
+            <div>
+            <p>Feels Like: {params.row.averageFeelsLike}°F</p>
+            <p>Temp Min: {params.row.averageTempMin}°F</p>
+            <p>Temp Max: {params.row.averageTempMax}°F</p>
+            <p>Pressure: {params.row.averagePressure} hPa</p>
+            <p>Humidity: {params.row.averageHumidity}%</p>
+            <p>Wind Speed: {params.row.averageWindSpeed} mph</p>
+            <p>Wind Gust: {params.row.averageWindGust} mph</p>
+            <p>Rain: {params.row.rainTotal} mm</p>
+            <p>Snow: {params.row.snowTotal} mm</p>
+            <p>Main: {params.row.weatherMain}</p>
+            <p>Description: {params.row.weatherDescription}</p>
+          </div>
+        );
+
+          return (
+            <Tooltip title={tooltipContent} arrow>
+              <img src={iconUrl} alt="Weather Icon" style={{ width: '30px', height: '30px', marginLeft: '10px' }} />
+            </Tooltip>
+          );
+        }
+        return null;
+      },
     },
   ];
 
-  // Fetch data using the custom hook useParseCSV
-  const { data, loading, error } = useParseCSV();
-
-  // Loading and error handling for the CSV data
-  if (loading) return <Box m="20px">Loading...</Box>;
-  if (error) return <Box>Error loading data: {error.message}</Box>;
-
-  // Function to parse a date string into a Date object
-  const parseDate = (dateString) => {
-    // Splitting the dateString into parts (year, month, day)
-    const parts = dateString.split("-");
-    // Check if dateString format is correct (YYYY-MM-DD)
-    if (parts.length === 3) {
-      // Parse each part into integers and construct a Date object
-      const year = parseInt(parts[0], 10);
-      const month = parseInt(parts[1], 10) - 1;
-      const day = parseInt(parts[2], 10);
-      return new Date(year, month, day); // Return a new Date object constructed from year, month, and day
-    }
-    // Return null if format is incorrect
-    return null;
-  };
-
-  // Extract unique years from the data for the dropdown
   const years = Array.from(
-    new Set(
-      data
-        .map((item) => {
-          const date = parseDate(item.Date);
-          return date ? date.getFullYear() : null;
-        })
-        .filter(Boolean)
-    )
+    new Set(fetchedData.map((item) => new Date(item.Date).getFullYear()))
   ).sort();
 
-  // Function to handle year selection change
   const handleYearChange = (event) => {
     setSelectedYear(event.target.value);
   };
 
-  // Function to group data by month and year
   const groupDataByMonth = (data) => {
     const monthlyData = {};
-    data.forEach((item, index) => {
-      const parsedDate = parseDate(item.Date);
-      /// Exclude data not matching the selected year
+    data.forEach((item) => {
+      const parsedDate = new Date(item.Date + "T00:00:00");
       if (selectedYear && parsedDate.getFullYear() !== selectedYear) {
         return;
       }
-      // Format the date to a readable Month-Year string
       const monthYear = parsedDate.toLocaleString("default", {
         month: "long",
         year: "numeric",
       });
-      // Initialize the array for each month-year if it doesn't exist
       if (!monthlyData[monthYear]) {
         monthlyData[monthYear] = [];
       }
-      // Push the item into the respective month-year array
-      monthlyData[monthYear].push({
-        ...item,
-        uniqueId: `${monthYear}-${index}`, // Generating a unique ID
-      });
+      monthlyData[monthYear].push(item);
     });
     return monthlyData;
   };
 
-  // Generating a unique ID
   const calculateYTDTotals = (data) => {
-    if (!selectedYear) return 0; // Return 0 if no year is selected
-    // Use the reduce function to iterate over each item in the data array
+    if (!selectedYear) return 0;
     return data.reduce((sum, item) => {
-      const parsedDate = parseDate(item.Date);
-      // Sum up 'Rounds Played' for the selected year
-      if (parsedDate && parsedDate.getFullYear() === selectedYear) {
-        const roundsPlayed = parseInt(item["Rounds Played"], 10);  //Radix specified as 10 to avoid unexpected
-        return sum + (isNaN(roundsPlayed) ? 0 : roundsPlayed);
+      const parsedDate = new Date(item.Date);
+      if (parsedDate.getFullYear() === selectedYear) {
+        return sum + item["Rounds Played"];
       }
       return sum;
     }, 0);
   };
 
-  // Function to render each month's DataGrid
   const renderMonthDataGrid = (monthYear, monthData, totalRoundsPlayed) => {
-    // Prepare data for rendering, including a total row for each month
     const dataWithTotal = [
       ...monthData,
       {
-        uniqueId: `${monthYear}-total`,
+        id: `${monthYear}-total`,
         Date: "Total",
         Day: "",
         "Rounds Played": totalRoundsPlayed,
+        "Weather Icon": "",
       },
     ];
 
@@ -137,7 +150,6 @@ const History = () => {
           </Typography>
           <Box
             sx={{
-              // Styling for the DataGrid component
               "& .MuiDataGrid-root": {
                 border: `1px solid ${colors.grey[300]}`,
               },
@@ -162,7 +174,7 @@ const History = () => {
               columns={columns}
               pageSize={6}
               rowsPerPageOptions={[6]}
-              getRowId={(row) => row.uniqueId}
+              getRowId={(row) => row.id}
             />
           </Box>
         </Box>
@@ -170,12 +182,9 @@ const History = () => {
     );
   };
 
-  // Rendering the History component
   return (
     <Box m="20px">
       <Header title="HISTORICAL DATA" subtitle="Daily Rounds Played Archive" />
-
-      {/* Dropdown selector for year filter */}
       <Box mb={4}>
         <FormControl fullWidth>
           <InputLabel id="year-select-label">Year</InputLabel>
@@ -195,21 +204,19 @@ const History = () => {
           </Select>
         </FormControl>
       </Box>
-
-      {/* Display YTD total box if a year is selected */}
       {selectedYear && (
         <Box mb={4} p={2} bgcolor={colors.blueAccent[700]} color="white">
           <Typography variant="h2" textAlign="center">
-            Total Rounds Played in {selectedYear}: {calculateYTDTotals(data)}
+            Total Rounds Played in {selectedYear}:{" "}
+            {calculateYTDTotals(fetchedData)}
           </Typography>
         </Box>
       )}
-      {/* Render DataGrids for each month */}
       <Grid container spacing={2}>
-        {Object.entries(groupDataByMonth(data)).map(
+        {Object.entries(groupDataByMonth(fetchedData)).map(
           ([monthYear, monthData]) => {
             const totalRoundsPlayed = monthData.reduce(
-              (sum, item) => sum + parseInt(item["Rounds Played"], 10),
+              (sum, item) => sum + item["Rounds Played"],
               0
             );
             return renderMonthDataGrid(monthYear, monthData, totalRoundsPlayed);
