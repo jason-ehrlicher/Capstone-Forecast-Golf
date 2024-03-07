5e6d71645ee3f4d84cf5d25b68194ecaf30cb625
Jason Ehrlicher <jehrlich@uno.edu>
2024-03-06 22:26:16 -0600

    added simulated reports page

--- mini-project2/src/scenes/reports/index.jsx
+++ mini-project2/src/scenes/reports/index.jsx
@@ -1,14 +1,26 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
+import AverageRoundsBarChart from "../../components/AverageRoundsBarChart";
+import MonthLineChart from "../../components/MonthlyRoundsLinechart";
+import Heatmap from "../../components/Heatmap";
+
+
 
 // Reports component definition
 const Reports = () => {
   // Rendering the Reports component
   return (
+    <>
     <Box m="20px">
       {/* Header component with a title and subtitle */}
       <Header title="REPORTS" subtitle="Insights from Your Data" />
     </Box>
+    <Box m="20px">
+    <AverageRoundsBarChart />
+      <MonthLineChart />
+      <Heatmap />
+    </Box>
+    </>
   );
 };
 
